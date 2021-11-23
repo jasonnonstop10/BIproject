2e280d2a38afa3e45219ab81d931cf627d5b5f04
jasonnonstop10 <jay44411@gmail.com>
2021-11-24 01:02:29 +0700

    [add] add cors

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI
 import stock_detall as sd
 import stock_statics as ss
-
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "https://bipython.herokuapp.com/",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 stock = ["ADVANC", "AOT", "BBL", "BDMS", "BEM", "BGRIM", "BH", "BJC", "BTS", "CBG", "COM7", "CPALL", "CPF", "CPN",
          "CRC", "DELTA", "DTAC", "EA", "EGCO", "GLOBAL", "GPSC", "GULF", "HMPRO", "INTUCH", "IRPC", "IVL", "KBANK", "KCE", "KTB", "KTC", "LH", "MINT", "MTC", "OR", "OSP", "PTT", "PTTEP", "PTTGC", "RATCH", "SAWAD", "SCB", "SCC", "SCGP", "STA", "STGT", "TISCO", "TOP", "TRUE", "TU"]
 
